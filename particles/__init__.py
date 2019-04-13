39239f9786c6755a6cd3d11cb7786a65d2c95396
Michael Zhang <mzhang2016@gmail.com>
2019-04-13 15:02:50 -0400

    Register personalization environment

--- particles/__init__.py
+++ particles/__init__.py
@@ -1,5 +1,18 @@
 from gym.envs.registration import register
 
 # Personalized Particle Envs
+# ----------------------------------------
 
-raise NotImplementedError
+register(
+    id='PersonalAgentSimple-v0',
+    entry_point='particles.envs:SimpleEnv',
+    # FIXME(cathywu) currently has to be exactly max_path_length parameters in
+    # rllab run script
+    max_episode_steps=100,
+)
+
+# register(
+#     id='MultiagentSimpleSpeakerListener-v0',
+#     entry_point='multiagent.envs:SimpleSpeakerListenerEnv',
+#     max_episode_steps=100,
+# )
