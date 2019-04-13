0808aa8bff9cb17931132590bbbd514a52b09384
Michael Zhang <mzhang2016@gmail.com>
2019-04-13 14:01:44 -0400

    Update random reset for agents and landmarks

--- particles/scenarios/simple.py
+++ particles/scenarios/simple.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from particles.core import World, Agent, Population, Landmark
-from particlles.scenario import BaseScenario
+from particles.scenario import BaseScenario
 
 
 class Scenario(BaseScenario):
@@ -11,6 +11,8 @@ class Scenario(BaseScenario):
         self.population = Population(10, personalization='variance')
         self.num_agents = 1  # Number of agents at a time
         self.seed = 42  # Reproducibility
+        self.random_start = False
+        self.random_landmarks = False
 
     def make_world(self):
         world = World()
@@ -18,15 +20,15 @@ class Scenario(BaseScenario):
         world.agents = self._sample_agents()
         for i, agent in enumerate(world.agents):
             agent.collide = False
-            agent.state.p_pos = [-0.5, -0.5]
+            agent.state.p_pos = [-0.75, -0.75]
             agent.state.p_vel = np.zeros(world.dim_p)
         # Add landmarks
         world.landmarks = [Landmark() for i in range(1)]
-        for i, landmark = enumerate(world.landmarks):
+        for i, landmark in enumerate(world.landmarks):
             landmark.name = 'landmark-{}'.format(i)
             landmark.collide = False
             landmark.movable = False
-            landmark.state.p_pos = [0.5, 0.5]
+            landmark.state.p_pos = [0.25, 0.25]
             landmark.state.p_vel = np.zeros(world.dim_p)
         # Make initial conditions
         self.reset_world(world)
@@ -43,7 +45,7 @@ class Scenario(BaseScenario):
             selected_agents = self.population.agents
         return selected_agents
 
-    def reset_world(self, world, random_start=False, random_landmarks=True):
+    def reset_world(self, world):
         world.agents = self._sample_agents()
         # Because coloring
         for i, landmark in enumerate(world.landmarks):
@@ -53,25 +55,26 @@ class Scenario(BaseScenario):
         # Set random initial states
         for agent in world.agents:
             start_pos = (np.random.uniform(-1, 1, world.dim_p)
-                         if random_start else [-0.5, -0.5])
+                         if self.random_start else [-0.75, -0.75])
             agent.state.p_pos = start_pos
             agent.state.p_vel = np.zeros(world.dim_p)
 
         for landmark in world.landmarks:
             start_pos = (np.random.uniform(-1, 1, world.dim_p)
-                         if random_start else [+0.5, +0.5])
+                         if self.random_landmarks else [+0.25, +0.25])
             landmark.state.p_pos = start_pos
             landmark.state.p_vel = np.zeros(world.dim_p)
 
     def reward(self, agent, world):
         # Euclidean distance reward
-        dist2 = np.sum(np.square(agent.state.p_pos -
-                                 world.landmarks[0].state.p_pos))
+        dist2 = np.sum(np.square(np.array(agent.state.p_pos) -
+                                 np.array(world.landmarks[0].state.p_pos)))
         return -dist2
 
     def observation(self, agent, world):
         # get positions of all entities in this agent's reference frame
         entity_pos = []
         for entity in world.landmarks:
-            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+            entity_pos.append(np.array(entity.state.p_pos) -
+                              np.array(agent.state.p_pos))
         return np.concatenate([agent.state.p_vel] + entity_pos)
