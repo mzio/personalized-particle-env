328d8ee98c910446a05c0b74fa432b1d1b4f0149
Michael Zhang <mzhang2016@gmail.com>
2019-04-14 13:15:25 -0400

    Add visualization script

---
+++ enjoy.py
@@ -0,0 +1,78 @@
+# Run this to visualize trained policies
+
+import gym
+import csv
+import numpy as np
+import argparse
+
+import torch
+import torch.optim as optim
+
+import particles
+import particles.scenarios as scenarios
+from particles.environment import PersonalAgentEnv
+
+from models.policy import Reinforce
+
+parser = argparse.ArgumentParser(description=None)
+parser.add_argument('-s', '--scenario', default='simple.py',
+                    help='Path of the scenario Python script')
+parser.add_argument('-d', '--debug', action='store_true',
+                    help='Print for debugging')
+parser.add_argument('-p', '--personalization',
+                    help='Personalization setup: "variance", "remap", "none" supported')
+parser.add_argument('-e', '--episode_len', default=1000,
+                    type=int, help='Number of timesteps per episode')
+parser.add_argument('--seed', default=42, type=int,
+                    help='Randomization seed')
+parser.add_argument('--log_interval', default=1, type=int,
+                    help='Logging rate')
+parser.add_argument('--trained_model', default='model_0.pt',
+                    help='Trained model name for visualizaiton')
+args = parser.parse_args()
+
+scenario = scenarios.load(args.scenario).Scenario(
+    kind=args.personalization, seed=args.seed)
+# create world
+world = scenario.make_world()
+world.episode_len = args.episode_len
+
+env = PersonalAgentEnv(world, scenario.reset_world, scenario.reward,
+                       scenario.observation, info_callback=None,
+                       done_callback=scenario.done, shared_viewer=True)
+env.discrete_action_input = True
+
+env.render()
+
+policies = [Reinforce(env, i, env.observation_space[i].shape[0],
+                      env.action_space[0].n) for i in range(env.n)]
+
+policies[0].load_state_dict(torch.load(
+    './rc/run_001/trained_models/' + args.trained_model))
+
+eps = np.finfo(np.float32).eps.item()
+
+obs_n = env.reset()
+running_reward = 10
+
+rewards = [['Episode', 'Reward']]
+
+total_timesteps = 0
+
+while True:
+    t = 0
+    env.reset()
+    while t < args.episode_len:
+        act_n = []
+        ep_reward = 0
+        for i, policy in enumerate(policies):
+            act_n.append(policy.action(obs_n[i]))
+        obs_n, reward_n, done_n, _ = env.step(act_n)
+
+        env.render()
+
+        if done_n[i] is True:
+            env.reset()
+        t += 1
+
+# python enjoy.py --scenario simple.py --p 'none' --seed 0 --trained_model 'model-0.pt'
