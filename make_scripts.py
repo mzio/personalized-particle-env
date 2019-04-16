2339b77637d355250acad30e3799c5a098c70b3a
Michael Zhang <mzhang2016@gmail.com>
2019-04-16 05:18:50 -0400

    Update batch script generator for occupancy measure

--- make_scripts.py
+++ make_scripts.py
@@ -1,8 +1,10 @@
-for i in range(10):
-    fname = './run_scripts/ppe_simple_reinforce_{}.sh'.format(i)
-    job_id = 'ppe_simple_reinforce_{}'.format(i)
-    with open(fname, 'w') as rsh:
-        rsh.write('''\
+for p in range(11):
+    name = 'PersonalAgent-' + str(p) if p < 10 else ''
+    for i in range(10):
+        fname = './run_scripts/ppe_simple_actor-critic_{}.sh'.format(i)
+        job_id = 'ppe_simple_actor-critic_{}'.format(i)
+        with open(fname, 'w') as rsh:
+            rsh.write('''\
 #!/bin/bash
 #SBATCH -J {}  # Job name
 #SBATCH -p fas_gpu               # Partition to submit to
@@ -18,7 +20,9 @@ source activate prl_env
 
 python main.py \
 --scenario simple.py \
---p 'none' --seed {} \
+--p 'variance' --seed {} \
 --save_results './results/results_{}.csv' \
---save_model './trained_models/model_{}.pt'
-'''.format(i, i, i))
+--save_model './trained_models/model_{}.pt'\
+--load_agents 'agents_many_10-1'\
+--specific_agents '{}'
+'''.format(job_id, job_id, job_id, i, job_id, job_id, name))
