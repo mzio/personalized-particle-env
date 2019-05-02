f5ce89fd507e605008929102b914a74d2d49cf62
Michael Zhang <mzhang2016@gmail.com>
2019-05-02 12:45:50 -0400

    Fix bugs for running

--- meta_train.py
+++ meta_train.py
@@ -16,6 +16,7 @@ import argparse
 
 import torch
 import torch.optim as optim
+from torch.autograd import Variable
 
 import particles
 import particles.scenarios as scenarios
@@ -109,32 +110,34 @@ else:
     raise NotImplementedError
 
 scenario.sample_task = True  # Start off true
-obs_n = env.reset()
+
 running_reward = -1.0
 
 total_timesteps = 0
 episode_ix = 0
 
 
-def inner_train(policy):
-    new_policy = model(0, env.observation_space[i].shape[0],
+def inner_train(policy, env=env):
+    new_policy = model(0, env.observation_space[0].shape[0],
                        env.action_space[0].n)
     new_policy.load_state_dict(torch.load(policy.state_dict()))
-    new_policy.finish_episode(optimizer, args.gamma)
+    # new_policy.rewards = policy.rewards
+    # new_policy.finish_episode(optimizer, args.gamma)
     for i in range(args.inner_updates):  # ex. 10, then do 10 times, update the average
         run_episode(new_policy)  # if 1 inner update, == SGD,
+        env.reset()
     new_policy.update(optimizer, args.inner_updates)
     return new_policy
 
 
-def run_episode(policy, obs=None, train=True):  # Call this K times
+def run_episode(policy, env=env, obs=None, train=True):  # Call this K times
     t = 0
+    obs_n = env.reset()
     while t < args.episode_len:
         ep_reward = 0
         act_n = []
-        for i, policy in enumerate(policies):
+        for i, p in enumerate(policies):
             act_n.append(policy.action(obs_n[i]))
-
         obs_n, reward_n, done_n, _ = env.step(act_n)
         policy.rewards.append(reward_n[0])
         ep_reward += reward_n[0]
@@ -142,7 +145,6 @@ def run_episode(policy, obs=None, train=True):  # Call this K times
             obs.append(obs_n[0])
         t += 1
     # Want to update the policy after
-    env.reset()
     if train:
         policy.finish_episode(optimizer, args.gamma)
     else:
@@ -151,7 +153,7 @@ def run_episode(policy, obs=None, train=True):  # Call this K times
 
 
 def inner_eval(policy, obs):
-    meta_reward = run_episode(policy, obs, train=False)
+    meta_reward = run_episode(policy, obs=obs, train=False)
     return meta_reward
 
 
@@ -159,7 +161,7 @@ def inner_eval(policy, obs):
 
 for n in range(args.num_updates):
     new_policy = policies[0]
-    for k in args.k:  # if K > 1, inner_updates = 1, optim = SGD, this is Reptile
+    for k in range(args.k):  # if K > 1, inner_updates = 1, optim = SGD, this is Reptile
         new_policy = inner_train(new_policy)
     type_observations = []
     # Evaluate
@@ -181,4 +183,4 @@ torch.save(policies[0].state_dict(), args.save_model)
 
 # python main.py --num_episodes 100 --p 'cluster' --seed 1 --save_results './results/results_ppe_simple_reinforce_sgd_5-1.csv' --save_model './trained_models/model_ppe_simple_reinforce_sgd_5-1.pt' --load_agents 'agents-clustered' --specific_agents 'PersonalAgent-5' --model 'Reinforce' --inner_updates 1 --log_interval 1 --episode_len 100
 
-# python meta_train.py --num_episodes 100 --seed 1 --save_results './results/results_ppe-joint_reptile-0.csv' --save_model './trained_models/model_ppe-joint_reptile-0.pt' --load_agents 'agents-clustered-p' --specific_agents 'PersonalAgent-0 PersonalAgent-1 PersonalAgent-2 PersonalAgent-3 PersonalAgent-4 PersonalAgent-5 PersonalAgent-8 PersonalAgent-9 PersonalAgent-10 PersonalAgent-11 PersonalAgent-12 PersonalAgent-15' --model 'Reinforce' --inner_updates 1 --k 1 --log_interval 1 --episode_len 100
+# python meta_train.py --num_episodes 100 --seed 1 --save_results './results/results_ppe-joint_reptile-0.csv' --save_model './trained_models/model_ppe-joint_reptile-0.pt' --load_agents 'agents-clustered-p' --specific_agents 'PersonalAgent-0 PersonalAgent-1 PersonalAgent-2 PersonalAgent-3 PersonalAgent-4 PersonalAgent-5 PersonalAgent-8 PersonalAgent-9 PersonalAgent-10 PersonalAgent-11 PersonalAgent-12 PersonalAgent-15' --model 'Reinforce' --inner_updates 1 --k 1 --log_interval 1 --episode_len 100 --optimizer 'SGD'
